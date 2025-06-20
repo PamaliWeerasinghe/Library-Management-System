ce719c1143c78153b4ca302c03abd96b989bd728
Pamali Weerasinghe <pamaliweerasinghe@gmail.com>
2025-06-20 16:18:22 +0530

    Altered the add new book

--- courselibrary/src/main/java/com/example/courselibrary/controller/BookController.java
+++ courselibrary/src/main/java/com/example/courselibrary/controller/BookController.java
@@ -73,4 +73,20 @@ public class BookController {
         return "redirect:/books";
     }
 
+    @GetMapping("/add-book")
+    public String addBook(Book book,Model model){
+        model.addAttribute("categories",categoryService.findAllCategories());
+        model.addAttribute("publishers",publisherService.findAllPublishers());
+        model.addAttribute("authors",authorService.findAllAuthors());
+        return "add-book";
+    }
+    @PostMapping("/save-book")
+    public String updateBook(Book book, BindingResult result,Model model){
+        if(result.hasErrors()){
+            return  "add-book";
+        }
+        bookService.createBook(book);
+        model.addAttribute("books",bookService.findAllBooks());
+        return "redirect:/books";
+    }
 }
